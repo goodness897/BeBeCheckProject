5264fdc72a2c1848ab6a9d85b678f9f0d2e36c4d
Museong <goodness897@gmail.com>
2016-10-14 17:29:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mu/bebecheckproject/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.mu.bebecheckproject;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentTabHost tabHost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initToolBar();
+        tabHost = (FragmentTabHost) findViewById(R.id.tabhost);
+        tabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("수유"), NursingFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("체온"), TemperatureFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("육아일기"), BabyBookFragment.class, null);
+
+        tabHost.setCurrentTabByTag("tab2");
+
+
+    }
+
+    private void initToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_noti) {
+
+        } else if (id == R.id.action_search) {
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private long backKeyPressedTime = 0;
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            Toast.makeText(this, "뒤로 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            finish();
+        }
+    }
+}
